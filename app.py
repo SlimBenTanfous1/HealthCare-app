b70d860c003789a80b952f5a63330487edda7d0e
Slimbentanfous1 <slim.bentanfous@esprit.tn>
2025-10-13 18:11:56 +0100

    changing to postgres

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
 
     # --- Config ---
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///healthcare.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "sqlite:///instance/healthcare.db"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "CHANGE_ME_IN_PROD")  # set in env in real deploy
 
